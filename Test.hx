32fcb2378b5ebd4093e07e412aef60a8075d7cb8
Davvex87 <dvvex87@gmail.com>
2025-04-22 16:18:53 +0100

    Unit Tests
    Replaces the old ugly Test.hx class with a new one that handles proper unit testing with assertion (utest library).

--- Test.hx
+++ Test.hx
@@ -1,131 +1,78 @@
 package;
 
-import hscript.AbstractHScriptClass;
+import utest.ITest;
+import utest.Assert;
+import hscript.*;
 
 class Test
 {
 	static function main()
 	{
-		// TODO: Make proper unit tests, this stinks
+		var runner = new utest.Runner();
 
-		var parser = new hscript.SuperParser();
-		var interp = new hscript.SuperInterp();
+		runner.addCase(new TestHScriptFeatures());
+		runner.addCase(new TestHScriptOOP());
 
-		var script:String = "
-		package hscript;
-
-		import haxe.ds.StringMap;
-		import haxe.io.Path;
-
-		class TestClass extends TestClass
-		{
-			public static var hscriptStaticVar:Float = 2.7;
-
-			static public function myStaticFunc(add:Float, mult:Float, div:Float = 1.3)
-			{
-				trace(hscriptStaticVar + add);
-				trace(hscriptStaticVar / div);
-				staticPrivateFunc();
-				return hscriptStaticVar * mult;
-			}
-
-
-
-
-			public var hscriptVar:String = 'Hello World!';
-
-			public function new(num:Int, str:String)
-			{
-				trace('instanced!');
-				trace(super);
-				super();
-				trace(num);
-				trace(str);
-				trace(super);
-			}
-
-			public function hscriptFunc(add:Float)
-			{
-				trace('hscript function called');
-				trace(super.myVar + add);
-			}
-
-			private function privateFunc()
-			{
-				trace('private func');
-				trace(super.myFunc('bruh', 555555.5555));
-			}
-
-			private static function staticPrivateFunc()
-			{
-				mult = 10; // going to ignore this, should not happen;
-				trace('called the private function from static class');
-			}
-		}
-		";
-		trace("created interp and parser");
-		
-		var program = parser.parseString(script, "TestScript.hx", 0 );
-		trace("parsed script");
-		parser.resumeErrors = true;
-		var declarations = parser.parseModule(script, "TestScript.hx", 0 );
-		parser.resumeErrors = false;
-		trace("parsed modules");
-
-		trace("executing");
-
-		interp.execute(program);
-
-		trace("registering structures");
-
-		interp.registerStructures(declarations);
-
-		trace("getting class");
-
-		var cls:AbstractHScriptClass = hscript.SuperInterp.resolveClass("hscript.TestClass");
-		trace("got the class");
-		
-		//try { trace(cls.hscriptStaticVar); } catch(e:Dynamic) { trace(e); } // works
-		//try { trace(cls.myStaticFunc(5, 3)); } catch(e:Dynamic) { trace(e); } // works, need to test to see if function arguments remain in the local variables list
-		//try { trace(cls.staticPrivateFunc()); } catch(e:Dynamic) { trace(e); } // works
-		// dont need to test for class extensions because static classes do not need to extend other classes
-
-		var inst:AbstractHScriptClass = cls.createInstance(85, "hi");
-
-		try { trace(inst.hscriptVar); } catch(e:Dynamic) { trace(e); }
-		try { trace(inst.hscriptFunc(2.5)); } catch(e:Dynamic) { trace(e); }
-		try { trace(inst.privateFunc()); } catch(e:Dynamic) { trace(e); }
-		try { trace(inst.myVar); } catch(e:Dynamic) { trace(e); }
-		try { trace(inst.myFunc(123,456)); } catch(e:Dynamic) { trace(e); }
-		try { trace(inst.super.myFunc(123,456)); } catch(e:Dynamic) { trace(e); }
-		try { inst.myVar += 8; } catch(e:Dynamic) { trace(e); }
-		try { trace(inst.myVar); } catch(e:Dynamic) { trace(e); }
-		try { trace(inst.myPrivateFunc(1)); } catch(e:Dynamic) { trace(e); }
-
-		trace("DONE!");
+		utest.ui.Report.create(runner);
+		runner.run();
 	}
 }
 
-class TestClass
+class TestHScriptFeatures implements ITest
 {
-	public static var staticVar:Float = 9.30;
+	public function new() {}
 
-	public var myVar:Int = 1;
-
-	public function new()
+	private function evalExpr(expr:String, ?params:Array<Dynamic>):Dynamic
 	{
-		trace("CONSTRUCTOR CALLED!!!");
+		var parser = new SuperParser();
+		var interp = new SuperInterp();
+
+		var program = parser.parseString(expr, '<eval>', 0);
+
+		if (params != null)
+			interp.variables.set("params", params);
+
+		return interp.execute(program);
 	}
 
-	public function myFunc(arg1:Dynamic, arg2:Dynamic, arg3:Dynamic)
+	function testArithmetic()
 	{
-		trace('my function was called with args $arg1 $arg2 and $arg3');
-		return 5;
+		Assert.equals(7, evalExpr('3 + 4;'));
+		Assert.equals(14, evalExpr('3 * 4 + 2;'));
+		Assert.equals(4, evalExpr('16 / (7 - 3);'));
 	}
 
-	private function myPrivateFunc()
+	function testConditions()
 	{
-		trace('my private function was called');
-		return 'hello!';
+		Assert.isTrue(evalExpr("3 > 1;"));
+		Assert.isTrue(evalExpr("3 >= 3;"));
+		Assert.isFalse(evalExpr("4 < 7 && 1 + 2 == 4;"));
+		Assert.isTrue(evalExpr("false || 1 % 2 == 1;"));
+		Assert.equals("I am at work", evalExpr("if (true == false) 'I am here'; else 'I am at work';"));
+		Assert.equals(3, evalExpr("false ? 10 : 3;"));
 	}
+
+	function testStringInterpolation()
+	{
+		Assert.equals("Hello, World!", evalExpr("'Hello, ${params[0]}!';", ["World"]));
+		Assert.equals("I have 5 coins", evalExpr("'I have ${3+2} coins';"));
+		Assert.equals("OneTwoThree", evalExpr("'${params[0]}${params[1]}${params[2]}';", ["One","Two","Three"]));
+	}
+
+	function testAssignment()
+	{
+		Assert.equals(7, evalExpr('var a = 5; a += 2;'));
+		Assert.equals('foo', evalExpr("var a:String = null; a ??= 'foo'; a;"));
+	}
+
+	function testOptionalChaining()
+	{
+        Assert.equals('ok', evalExpr('var a = { f: "ok" }; a?.f;'));
+        Assert.isNull(evalExpr('var a = null; a?.f;'));
+    }
+}
+
+class TestHScriptOOP
+{
+	public function new() {}
 }
